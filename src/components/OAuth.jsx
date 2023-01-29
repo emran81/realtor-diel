251b138113cd2346971d0b4fb9516c2ab78c9de0
Syed Emran Ali <dielbn2013@gmail.cpm>
2023-01-29 14:11:16 +0600

    create sign in sign up forgot password and oauth component

---
+++ src/components/OAuth.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+
+export default function OAuth() {
+  return (
+    <button className=" flex items-center justify-center w-full lg:ml-20  bg-red-600 text-white px-7 py-4 text-sm font-medium uppercase rounded shadow-md hover:bg-red-700 transition duration-200 ease-in-out hover:shadow-xl active:bg-red-900">
+      <FcGoogle className="text-2x1 bg-white rounded-full mr-2" /> Continue with
+      Google
+    </button>
+  );
+}
